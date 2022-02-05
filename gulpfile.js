33a0ed9d8f161bcd67ace77761ea34f38ba07c9d
Maksim Chekhovskiy <veles.cafe@gmail.com>
2022-02-05 19:52:27 +0200

    e

--- gulpfile.js
+++ gulpfile.js
@@ -44,8 +44,8 @@ function scripts() {
 }
 
 function styles() {
-    return src('app/scss/style.scss'
-)
+    return src(['node_modules/slick-carousel/slick/slick.scss',
+        'app/scss/style.scss'])
         .pipe(scss({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
